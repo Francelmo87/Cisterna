d7cbb9b6d867a7a2bd503603d6173a635e4a39fb
Francelmo87 <francelmo87@hotmail.com>
2024-01-30 16:56:34 -0300

    inserida o inline da cisterna

--- backend/usuarios/admin.py
+++ backend/usuarios/admin.py
@@ -1,26 +1,25 @@
 from django.contrib import admin
 
-from .models import Titular, Endereco, Membro
+from .models import Titular, Endereco, Membro, Cisterna
 
 
 class MembroInline(admin.TabularInline):
     model = Membro
     extra = 0
-    # list_display = ('nome', 'cpf', 'parentesco', 'idade', 'ocupacao', 'renda', 'usuario',)
 
 
 class EnderecoInline(admin.TabularInline):
     model = Endereco
     extra = 0
-    # list_display = ('distrito', 'localidade', 'geolocalizacao', 'usuario',)
+
+
+class CisternaInline(admin.TabularInline):
+    model = Cisterna
+    extra = 0
 
 
 @admin.register(Titular)
 class UsuarioAdmin(admin.ModelAdmin):
-    inlines = (MembroInline, EnderecoInline)
-    list_display = ('inicio', 'fim', 'nome', 'rg', 'cpf', 'nis', 'telefone', 'active',)
-
-
-
-
+    inlines = (MembroInline, EnderecoInline, CisternaInline)
+    list_display = ('nome', 'rg', 'cpf', 'nis', 'telefone', 'active',)
 
