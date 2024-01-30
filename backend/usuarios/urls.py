493a7a9f3807d122d2ee4d894cbb7373c964383e
Francelmo87 <francelmo87@hotmail.com>
2024-01-30 17:10:09 -0300

    Retirada a urls desnecessarios

--- backend/usuarios/urls.py
+++ backend/usuarios/urls.py
@@ -7,7 +7,7 @@ app_name = 'usuarios'
 urlpatterns = [
    path('membro_list/<int:pk>/', v.membro_list, name='membro_list'),
    path('endereco_titular/<int:pk>/', v.endereco_titular, name='endereco_titular'),
+   path('titular_detail/<int:pk>/', v.titular_detail, name='titular_detail'),
    path('', v.titular_list, name='titular_list'),
-   path('<int:pk>/', v.titular_detail, name='titular_detail'),
 
 ]
